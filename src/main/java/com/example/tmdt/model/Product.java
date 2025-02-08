445fcffeb62afcf6099cca2682f7fef5baf610c2
locpham92 <phamquyloc92@gmail.com>
2025-02-08 23:09:06 +0700

    loc bo sung class Product

--- src/main/java/com/example/tmdt/model/Product.java
+++ src/main/java/com/example/tmdt/model/Product.java
@@ -5,14 +5,16 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
-
 @Entity
 @Data
-public class Seller {
+public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
-    private String username;
-    private String password;
+    private double price;
+    private int quantity;
+    private String image;
+    private String description;
 }
+
