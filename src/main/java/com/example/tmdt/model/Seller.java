d762f8034fd91a1ee8f2140ce5278466c1447546
locpham92 <phamquyloc92@gmail.com>
2025-02-08 10:36:51 +0700

    loc update Seller trong model

---
+++ src/main/java/com/example/tmdt/model/Seller.java
@@ -0,0 +1,19 @@
+package com.example.tmdt.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Seller {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String username;
+    private String password;
+
+}
